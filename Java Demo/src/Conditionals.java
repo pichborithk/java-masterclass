2fa2da71b403d1388c566851ea816e60bd3d88d4
Pichborith Kong <pichborith@gmail.com>
2023-09-24 19:16:43 -0700

    update Java Demo

--- Java Demo/src/Conditionals.java
+++ Java Demo/src/Conditionals.java
@@ -7,5 +7,31 @@ public class Conditionals {
         if (!isAlien) {
             System.out.println("It is not an alien");
         }
+
+        int score = 95;
+        if (score == 100) {
+            System.out.println("A+");
+        } else if (score < 100 && score >= 90) {
+            System.out.println('A');
+        } else {
+            System.out.println("Pass");
+        }
+
+        if (score > 100 || isAlien) {
+            System.out.println("It is an alien");
+        }
+
+        // Ternary operator
+        boolean isAlien2 = score > 100 ? true : false;
+        System.out.println(isAlien2);
+        boolean isAlien3 = score > 100;
+        System.out.println(isAlien3);
+
+        String grade1 = score == 100
+                ? "A+"
+                : score < 100 && score >= 90
+                ? "A"
+                : "Pass";
+        System.out.println(grade1);
     }
 }
