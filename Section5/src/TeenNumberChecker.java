04709721c761692d452016783f85f300484dccc5
Pichborith Kong <pichborith@gmail.com>
2023-09-27 15:57:02 -0700

    section 5 code exercises

---
+++ Section5/src/TeenNumberChecker.java
@@ -0,0 +1,13 @@
+public class TeenNumberChecker {
+
+    public static void main(String[] args) {
+        System.out.println(hasTeen(25, 15, 39));
+    }
+    public static boolean hasTeen(int num1, int num2, int num3) {
+        return isTeen(num1) || isTeen(num2) || isTeen(num3);
+    }
+
+    public static boolean isTeen(int num) {
+        return num >= 13 && num <= 19;
+    }
+}
