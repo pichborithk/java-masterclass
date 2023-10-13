4dbacdae7084b119f2d7e25979735df4ec38bde3
Pichborith Kong <pichborith@gmail.com>
2023-10-13 16:48:52 -0700

    update section 6

---
+++ Section6/src/InputCalculator.java
@@ -0,0 +1,30 @@
+public class InputCalculator {
+
+    public static void main(String[] args) {
+        inputThenPrintSumAndAverage();
+    }
+
+    public static void inputThenPrintSumAndAverage() {
+        int sum = 0;
+        double counter = 0;
+        int average = 0;
+        while (true) {
+            String input = System.console().readLine("Enter number: ");
+            try {
+                int number = Integer.parseInt(input);
+                sum += number;
+                counter++;
+            } catch (NumberFormatException badInputData) {
+                break;
+            }
+        }
+
+        if (counter > 0) {
+            average = (int) Math.round(sum / counter);
+        }
+
+        System.out.println("SUM = " + sum + " AVG = " + average);
+    }
+
+
+}
