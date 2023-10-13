8dd8e4ba8afedb67098e167e194e76c3d7cd1bff
Pichborith Kong <pichborith@gmail.com>
2023-10-12 21:44:37 -0700

    update section 6

---
+++ Section6/src/LargestPrime.java
@@ -0,0 +1,30 @@
+public class LargestPrime {
+
+    public static void main(String[] args) {
+        System.out.println(getLargestPrime(21));
+        System.out.println(getLargestPrime(217));
+    }
+
+    public static int getLargestPrime(int number) {
+        int result = -1;
+        for (int i = 1; i <= number / 2; i++) {
+            if (number % i == 0) {
+                int factor = number / i;
+                boolean isPrime = true;
+                for (int j = 2; j <= factor / 2; j++) {
+                    if (factor % j == 0) {
+                        isPrime = false;
+                        break;
+                    }
+                }
+
+                if (isPrime) {
+                    result = factor;
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+}
