a5b77dc59ed5136562d6df9017130e14cd57af32
Pichborith Kong <pichborith@gmail.com>
2023-10-24 22:16:52 -0700

    update demo

--- Java Demo/src/InterfaceDemo/OrbitEarth.java
+++ Java Demo/src/InterfaceDemo/OrbitEarth.java
@@ -1,6 +1,13 @@
 package InterfaceDemo;
 
+import java.util.Date;
+
 public interface OrbitEarth extends FlightEnabled{
 
     void achieveOrbit();
+
+    static void log(String description) {
+        var today = new Date();
+        System.out.println(today + ": " + description);
+    }
 }
