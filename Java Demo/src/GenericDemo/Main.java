c7e9ccda7a72b4ea269f1fcaf2cb671fb2c8afe7
Pichborith Kong <pichborith@gmail.com>
2023-10-25 18:20:48 -0700

    update demo

---
+++ Java Demo/src/GenericDemo/Main.java
@@ -0,0 +1,47 @@
+package GenericDemo;
+
+interface Player {
+
+    String name();
+}
+
+record BaseballPlayer(String name, String position) implements Player {
+}
+
+record FootballPlayer(String name, String position) implements Player {
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+        var philly = new Affiliation("city", "Philadelphia, PA", "US");
+
+        Team<BaseballPlayer, Affiliation> phillies = new Team<>("Philadelphia Phillies", philly);
+        Team<BaseballPlayer, Affiliation> astros = new Team<>("Houston Astros");
+        scoreResult(phillies, 3, astros, 5);
+
+        var harper = new BaseballPlayer("B Harper", "Right Fielder");
+        var marsh = new BaseballPlayer("B Marsh", "Right Fielder");
+        phillies.addTeamMember(harper);
+        phillies.addTeamMember(marsh);
+        var guthrie = new BaseballPlayer("D Guthrie", "Center Fielder");
+        phillies.addTeamMember(guthrie);
+        phillies.listTeamMembers();
+
+        Team<FootballPlayer, String> afc = new Team<>("Adelaide Crows",
+                "City of Adelaide, South Australia, in AU");
+        var tex = new FootballPlayer("Tex Walker", "Centre half forward");
+        afc.addTeamMember(tex);
+        var rory = new FootballPlayer("Rory Laird", "Midfield");
+        afc.addTeamMember(rory);
+        afc.listTeamMembers();
+    }
+
+    public static void scoreResult(Team team1, int t1_score,
+                                   Team team2, int t2_score) {
+
+        String message = team1.setScore(t1_score, t2_score);
+        team2.setScore(t2_score, t1_score);
+        System.out.printf("%s %s %s %n", team1, message, team2);
+    }
+}
